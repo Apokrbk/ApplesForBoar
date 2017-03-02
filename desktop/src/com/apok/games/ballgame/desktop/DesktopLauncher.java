074cec0fc8213f4761f27ff7668816b9524de92c
Apokrbk <damian.portasinski@gmail.com>
2017-03-02 22:21:30 +0100

    Init commit

---
+++ desktop/src/com/apok/games/ballgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.apok.games.ballgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.apok.games.ballgame.BallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallGame(), config);
+	}
+}

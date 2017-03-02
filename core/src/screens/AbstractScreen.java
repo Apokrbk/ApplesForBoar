655bf0f562497bfbf68b81ae26e7c3c7f3e3c7d0
Apokrbk <damian.portasinski@gmail.com>
2017-03-02 22:32:22 +0100

    Added AbstractScreen class

---
+++ core/src/screens/AbstractScreen.java
@@ -0,0 +1,80 @@
+package screens;
+
+import com.apok.games.ballgame.BallGame;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+/**
+ * Created by Apok on 02.03.2017.
+ */
+
+public abstract class AbstractScreen implements Screen {
+    protected BallGame game;
+    protected Stage stage;
+    private OrthographicCamera camera;
+    protected SpriteBatch spriteBatch;
+
+    public AbstractScreen(BallGame game)
+    {
+        this.game = game;
+        createCamera();
+        stage = new Stage(new StretchViewport(BallGame.WIDTH, BallGame.HEIGHT, camera));
+        spriteBatch = new SpriteBatch();
+        Gdx.input.setInputProcessor(stage);
+        init();
+    }
+
+    protected abstract void init();
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    private void createCamera()
+    {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, BallGame.WIDTH, BallGame.HEIGHT);
+        camera.update();
+    }
+
+    @Override
+    public void render(float delta) {
+        clearScreen();
+        camera.update();
+        spriteBatch.setProjectionMatrix(camera.combined);
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(0,0,0,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void dispose() {
+        game.dispose();
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}

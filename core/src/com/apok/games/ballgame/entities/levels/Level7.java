c71dcd897ab706d30ec679c49d2658c214992311
Apokrbk <damian.portasinski@gmail.com>
2017-03-07 18:26:08 +0100

    Added level7

--- core/src/com/apok/games/ballgame/entities/levels/Level7.java
+++ core/src/com/apok/games/ballgame/entities/levels/Level7.java
@@ -1,22 +1,18 @@
 package com.apok.games.ballgame.entities.levels;
 
-
 import com.apok.games.ballgame.entities.RotaryObstacle;
 import com.apok.games.ballgame.entities.SetOfObstacles;
 
-class Level6 extends SetOfObstacles {
+/**
+ * Created by Apok on 07.03.2017.
+ */
 
-    private static final int STARTING_X = 41;
-    private static final int STARTING_Y = 320;
-    private static final int WIDTH = 350;
-
-    Level6() {
-        super();
-    }
-
+public class Level7 extends SetOfObstacles{
     @Override
     protected void initObstacles() {
-        obstacles.add(new RotaryObstacle(STARTING_X, STARTING_Y, WIDTH, 0,0,2));
+        obstacles.add(new RotaryObstacle(0, 320, 216, 0,0, -2));
+        obstacles.add(new RotaryObstacle(216, 320, 216, 0,0, 2));
+
     }
 
     @Override
